7209985c1b68b8aa3053b0d19889777b13498492
vskec <119257086+vskec@users.noreply.github.com>
2023-03-25 20:04:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const clock = document.querySelector(".clock")
 
 function time() {
-    let date = new Date()//sve ovo je javascript ugrađena metoda; vraća 0 do 23,59,59
+    let date = new Date()
     let h = date.getHours()
     let m = date.getMinutes()
     let s = date.getSeconds()
 
-    h = (h < 10) ? "0" + h : h //ako je manje od 10 onda se dodaje 0 ispred sata
+    h = (h < 10) ? "0" + h : h 
     m = (m < 10) ? "0" + m : m
     s = (s < 10) ? "0" + s : s
 
     clock.innerText = h + " : " + m + " : " + s
-    const t = setTimeout(function() {time()}, 1000) //funkcija se update svaku sekundu, bez ovoga ne funkcionira
+    const t = setTimeout(function() {time()}, 1000) 
 }
 
 time()
